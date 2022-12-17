e5d54b5e9403cfc356f3ce3b701a6704e66c6f98
Nitesh <niteshalexa@gmail.com>
2022-12-17 16:59:07 +0530

    Initial commit

---
+++ HandTracking/VirtualMouse.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+import HandTrackingModuleFull as htm
+import time
+import autopy
+
+wCam , hCam = 640, 480
+
+cap = cv2.VideoCapture(1)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
