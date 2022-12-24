8cd43f61d5c55056d794dad8d1d5dab19bfddba2
Nitesh <niteshnunfara0@gmail.com>
2022-12-24 12:42:25 +0530

    Nitesh Kumar

---
+++ HandTracking/Brightness Control_clone.py
@@ -0,0 +1,72 @@
+import cv2
+import mediapipe as mp
+from math import hypot
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import numpy as np
+import time
+import screen_brightness_control as sbc
+
+
+
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volMin, volMax = volume.GetVolumeRange()[:2]
+pTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    lmList = []
+    if results.multi_hand_landmarks:
+        for handlandmark in results.multi_hand_landmarks:
+            for id, lm in enumerate(handlandmark.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmList.append([id, cx, cy])
+            mpDraw.draw_landmarks(img, handlandmark, mpHands.HAND_CONNECTIONS)
+
+    if lmList != []:
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        cx, cy = (x1 + x2) // 2, (y2 + y1) // 2
+        print(-y1)
+
+        cv2.circle(img, (x1, y1), 7, (255, 0, 0), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 7, (255, 0, 0), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 0), 3)
+
+        length = hypot(x2 - x1, y2 - y1)
+        # print("Length", length)
+####
+        if length < 30:
+            cv2.circle(img, (cx, cy), 7, (0, 255, 0), cv2.FILLED)
+            # print("CX CY: ", cx, cy)
+            vol = np.interp(-cy, [-350, -250], [20, 100])
+            # print(vol)
+            # volume.SetMasterVolumeLevel(vol, None)
+            sbc.fade_brightness(vol)
+
+        # Hand range 15 - 220
+        # Volume range -63.5 - 0.0
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
+
+    cv2.imshow('Image', img)
+    cv2.waitKey(1)
